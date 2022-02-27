00ef387b3acfec72018f67b4383c7d08e83c55d0
glive85 <glennstraughn@gmail.com>
2022-02-27 11:59:16 -0500

    third commit

--- app/src/main/java/com/example/glenn_straughn_c196_app/UI/TermList.java
+++ app/src/main/java/com/example/glenn_straughn_c196_app/UI/TermList.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Courses extends AppCompatActivity {
+public class TermList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_courses);
+        setContentView(R.layout.activity_term_list);
     }
 }
