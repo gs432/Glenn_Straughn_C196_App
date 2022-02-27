e43b07b1781578e43e31b3baba634cbc0faac0b5
glive85 <glennstraughn@gmail.com>
2022-02-27 15:20:11 -0500

    third commit

---
+++ app/src/main/java/com/example/glenn_straughn_c196_app/UI/CourseList.java
@@ -0,0 +1,14 @@
+package com.example.glenn_straughn_c196_app.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list);
+    }
+}
