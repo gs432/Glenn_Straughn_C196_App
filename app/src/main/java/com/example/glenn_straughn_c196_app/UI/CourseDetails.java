a9b8cc9422d45e987a7e32f11711c5d6ab75a0a2
glive85 <glennstraughn@gmail.com>
2022-03-07 23:11:19 -0500

    third commit

---
+++ app/src/main/java/com/example/glenn_straughn_c196_app/UI/CourseDetails.java
@@ -0,0 +1,16 @@
+package com.example.glenn_straughn_c196_app.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.glenn_straughn_c196_app.R;
+
+public class CourseDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+    }
+}
