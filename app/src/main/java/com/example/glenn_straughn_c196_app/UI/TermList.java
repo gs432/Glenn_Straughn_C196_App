459cedd8c9a37f47a0095ae146b298e3eb0c2351
glive85 <glennstraughn@gmail.com>
2022-03-19 20:38:54 -0400

    Constraints fixed

--- app/src/main/java/com/example/glenn_straughn_c196_app/UI/TermList.java
+++ app/src/main/java/com/example/glenn_straughn_c196_app/UI/TermList.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.glenn_straughn_c196_app.Database.Repository;
 import com.example.glenn_straughn_c196_app.R;
 
 public class TermList extends AppCompatActivity {
+    private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        repository = new Repository(getApplication());
     }
 }
